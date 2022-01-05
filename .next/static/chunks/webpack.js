c35a155116f4487ca5f2559c42c0927550a3ede8
Rebecca Combs <rdc47@duke.edu>
2022-01-05 00:57:48 -0500

    binary files changed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "67f8bf04ca32f485"; }
+/******/ 		__webpack_require__.h = function() { return "9000a8b0e501f95c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -19,7 +19,7 @@ export default function App({ Component, pageProps }) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <title>ThereUGo. | Learn Faster. Understand Better.</title>
+        <title>ThereUgo. | Learn Faster. Understand Better.</title>
       </Head>
       <Page>
         <WebBasic>
